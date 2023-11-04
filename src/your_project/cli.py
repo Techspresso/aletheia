31dcf180bb6e85efa4b20215dbf053ad45a4cc5f
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-11-04 15:31:15 +0000

    feat: add get_analysis function to cli

--- src/your_project/cli.py
+++ src/your_project/cli.py
@@ -5,7 +5,16 @@ from your_project.get_article import scrape_with_playwright, article_content_ext
 def main():
     if len(sys.argv) > 1:
         urls = sys.argv[1:]
-        extracted_content = scrape_with_playwright(urls, schema=article_content_extraction_schema)
-        print(extracted_content)
+        comparative_analysis = get_analysis(urls[0])
+        print(comparative_analysis)
     else:
         print("Please provide a URL as a command line argument.")
+
+def get_analysis(url):
+    article = scrape_with_playwright([url], schema=article_content_extraction_schema)
+    topic = get_topic(article)
+    similar_articles = get_similar_articles(article, topic)
+    analyzed_articles = analyze_articles([article, *similar_articles], topic)
+    comparative_analysis = get_comparative_analysis(analyzed_articles, topic)
+    return comparative_analysis
+
