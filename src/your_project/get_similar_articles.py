6935873786fa786d43ef77aed4c07b7fd3a47a3f
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-11-04 18:25:27 +0000

    get articles on topic

--- src/your_project/get_similar_articles.py
+++ src/your_project/get_similar_articles.py
@@ -1,18 +1,20 @@
 from langchain.document_loaders import BraveSearchLoader
 
-from your_project.get_article import get_article
+from your_project.get_article import get_article_content
 
 api_key = "BSAt2nmuC57jmjrGEY9-JNAyAHTU6Z5"
 
-def get_articles_on_topic(topic, count=3):
+def get_urls_on_topic(topic, count=3):
     docs = search(topic, count=count)
     urls = [doc.metadata["link"] for doc in docs]
     return urls
 
 def get_articles_from_urls(urls):
-    articles = [get_article(url) for url in urls]
+    articles = get_article_content(urls)
     return articles
 
+def get_articles_on_topic(topic, count=3):
+    return get_articles_from_urls(get_urls_on_topic(topic, count=count))
 
 def search(topic, count=3):
     loader = BraveSearchLoader(
@@ -21,4 +23,4 @@ def search(topic, count=3):
     return loader.load()
 
 if __name__ == "__main__":
-    print(get_articles_on_topic("hamas"))
+    print(get_articles_from_urls(get_urls_on_topic("hamas and isreal conflict")))
