6c68a56e86406e188a20fd7050cf6a3e6aef1a9f
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-11-05 03:00:07 +0000

    d /Users/l/Workspace/code/simple-python-template ; /usr/bin/env /Users/l/Workspace/code/simple-python-template/venv/bin/python /Users/l/.vscode/extensions/ms-python.python-2023.20.0/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 56425 -- /Users/l/Workspace/code/simple-python-template/src/aletheia/get_article.py

--- src/aletheia/get_article.py
+++ src/aletheia/get_article.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from langchain.chains import create_extraction_chain
 import pprint
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -9,10 +10,16 @@ from langchain.prompts import ChatPromptTemplate
 from aletheia import llm
 from aletheia.models import Article
 
-def get_article_content(urls):
+def get_title_from_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    return soup.find("h1").text
+
+def get_article_content(url):
+    urls = [url]
     loader = AsyncChromiumLoader(urls)
     docs = loader.load()
     bs_transformer = BeautifulSoupTransformer()
+    title = get_title_from_html(docs[0].page_content)
     docs_transformed = bs_transformer.transform_documents(
         docs, tags_to_extract=["article", "h1"], unwanted_tags=["aside"]
     )
@@ -21,17 +28,13 @@ def get_article_content(urls):
         chunk_size=80000, chunk_overlap=0
     )
     splits = splitter.split_documents(docs_transformed)
-    title = extract_titles_from_urls(urls)
-    print("Title: " + str(title))
-
     print(f"Got article content for urls: {urls}")
-    return [{"content": split.page_content, "title": title, "url": url} for split, url, title in zip(splits,urls,title)]
+    return {"content": splits[0].page_content, "url": url, "title": title}
 
 
-'''if __name__ == "__main__":
+if __name__ == "__main__":
     urls = [
         "https://www.theguardian.com/world/live/2023/oct/31/israel-hamas-war-live-updates-latest-news-today-hamas-clashes-idf-gaza-aid-plan-failure",
     ]
     content = get_article_content(urls)
     print(content)
-'''
