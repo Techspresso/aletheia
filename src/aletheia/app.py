95f9dcadff52c6c8828c057c1afb349586950b92
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-11-12 18:40:24 +0000

    chore: cleanup imports

--- src/aletheia/app.py
+++ src/aletheia/app.py
@@ -1,15 +1,20 @@
-import pprint
-from flask import Flask
-from flask import request, jsonify
-import json
 import base64
-from aletheia import get_similar_articles
-from aletheia.analysis import getKeyPointsClaude, getBiasClaude, getArticleAnalysis, getArticleTopic, getAntiTopic, checkIfAnti
-from aletheia.get_article import get_article_content
-from aletheia.models import Article
+import pprint
 
+from flask import Flask, request
+
+from aletheia import get_similar_articles
+from aletheia.analysis import (
+    checkIfAnti,
+    getAntiTopic,
+    getArticleTopic,
+    getBiasClaude,
+    getKeyPointsClaude,
+)
+from aletheia.get_article import get_article_content
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -19,30 +24,33 @@ def index():
         return "No search query provided"
     print()
 
-    url = base64.b64decode(query).decode() #decode later
+    url = base64.b64decode(query).decode()  # decode later
     print("Fetching article from url: " + url + "\n")
     cur_content = get_article_content([url])[0]
     print(cur_content)
-    topic = getArticleTopic(cur_content['content'])
+    topic = getArticleTopic(cur_content["content"])
     print("Detected topic: " + topic + "\n")
     isAnti = checkIfAnti(topic)
-    if(isAnti):
+    count = 3
+    if isAnti:
         anti_topic = getAntiTopic(topic)
         print("Generated Augmented Topic: " + anti_topic + "\n")
-    same_articles = get_similar_articles.get_articles_on_topic(topic, url, count=2)
+    same_articles = get_similar_articles.get_articles_on_topic(topic, url, count=count)
     diff_articles = []
-    if(isAnti):
-        diff_articles = get_similar_articles.get_articles_on_topic(anti_topic, url, count=2)
+    if isAnti:
+        diff_articles = get_similar_articles.get_articles_on_topic(
+            anti_topic, url, count=count
+        )
 
     anal_json = {}
     anal_json["articles"] = []
 
     cur_dict = {}
     cur_dict["topic"] = topic
-    cur_dict["key_points"] = getKeyPointsClaude(cur_content['content'])
+    cur_dict["key_points"] = getKeyPointsClaude(cur_content["content"])
     cur_dict["url"] = url
-    cur_dict["title"] = cur_content['title']
-    cur_dict["bias"] = getBiasClaude(cur_content['content'], topic)
+    cur_dict["title"] = cur_content["title"]
+    cur_dict["bias"] = getBiasClaude(cur_content["content"], topic)
     anal_json["articles"].append(cur_dict)
     print("Obtained article analysis for the original article")
     pprint.pprint(cur_dict)
@@ -51,10 +59,10 @@ def index():
     for article in same_articles:
         cur_dict = {}
         cur_dict["topic"] = topic
-        cur_dict["key_points"] = getKeyPointsClaude(article['content'])
-        cur_dict["url"] = article['url']
-        cur_dict["title"] = article['title']
-        cur_dict["bias"] = getBiasClaude(article['content'], topic)
+        cur_dict["key_points"] = getKeyPointsClaude(article["content"])
+        cur_dict["url"] = article["url"]
+        cur_dict["title"] = article["title"]
+        cur_dict["bias"] = getBiasClaude(article["content"], topic)
         anal_json["articles"].append(cur_dict)
         print("Obtained article analysis:")
         pprint.pprint(cur_dict)
@@ -63,24 +71,13 @@ def index():
     for article in diff_articles:
         cur_dict = {}
         cur_dict["topic"] = anti_topic
-        cur_dict["key_points"] = getKeyPointsClaude(article['content'])
-        cur_dict["url"] = article['url']
-        cur_dict["title"] = article['title']
-        cur_dict["bias"] = getBiasClaude(article['content'], anti_topic)
+        cur_dict["key_points"] = getKeyPointsClaude(article["content"])
+        cur_dict["url"] = article["url"]
+        cur_dict["title"] = article["title"]
+        cur_dict["bias"] = getBiasClaude(article["content"], anti_topic)
         anal_json["articles"].append(cur_dict)
         print("Obtained article analysis:")
         pprint.pprint(cur_dict)
         print()
 
     return anal_json
-
-
-        
-    
-
-
-
-     
-    
-    
-    
